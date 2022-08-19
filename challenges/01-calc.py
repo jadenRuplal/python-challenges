50960fa954fa1494cea9cd11b3a5a0c6ce25f867
jadenruplal <jadenruplal@gmail.com>
2022-08-18 21:40:53 -0400

    all but part four fully working

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,23 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+def calculator():
+
+  
+    first = int(input("first number\n"))
+    sign = input("What method would you like to use?\n")
+    second = int(input("second number\n"))
+
+  signs = {
+    "add": first + second,
+    "sub": first - decond,
+    "multiply": first * second,
+    "div": first / second,
+  }
+
+  if (sign in signs):
+    return signs[sign]
+
+print(calculator())
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,26 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+finished = 'yes'
+balance = 0
+def bank():
+global finished
+global balance
+  while (finished != "yes"):
+    ans = input("What would you like to do? (deposit, withdrawl, check_balance\n")
+
+    if (ans == "deposit"):
+      amount = int(input("How much would you like to deposit?\n"))
+      balance = balance + amount
+      print(f"Your current balance is {balance}\n")
+      finished = input("Are you done?\n")
+    elif (ans == "withdraw"):
+      amount = int(input("How much would you like to withdraw?\n"))
+      balance = balance - amount
+      print(f"Your current balance is {balance}\n")
+      finished = input("Are you done?\n")
+    elif (ans == "balance"):
+      print(f"Your current balance is {balance}\n")
+      finished = input("Are you finished?\n")
+      
+bank()
